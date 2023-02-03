0c5e9aa4581198185ff77270a5f679c11c75aa16
EricBorder <magicsergy@gmail.com>
2023-02-03 13:17:38 +0100

    Terminado Fragmento de la suma

--- app/src/main/java/com/example/tareafragments/ui/main/SumaFragment.kt
+++ app/src/main/java/com/example/tareafragments/ui/main/SumaFragment.kt
@@ -7,6 +7,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.example.tareafragments.R
 
 class SumaFragment : Fragment() {
@@ -21,6 +25,41 @@ class SumaFragment : Fragment() {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+        val miModelo by viewModels<MainViewModel>()
+
+        miModelo.liveRandom.observe(
+            this,
+            Observer(
+                fun(random: Int) {
+                    var tvRandom: TextView? = view?.findViewById(R.id.sumaRandom1)
+                    tvRandom?.setText("$random")
+
+                }
+            )
+
+        )
+        miModelo.liveRandom2.observe(
+            this,
+            Observer(
+                fun(random2: Int) {
+                    var tvRandom2: TextView? = view?.findViewById(R.id.sumaRandom2)
+                    tvRandom2?.setText("$random2")
+
+                }
+            )
+
+        )
+
+        miModelo.liveResultado.observe(
+            this,
+            Observer(
+                fun(resultado: Int) {
+                    var tvResultado: TextView? = view?.findViewById(R.id.sumaResultado)
+                    tvResultado?.setText("$resultado")
+
+                }
+            )
+        )
 
     }
 
@@ -29,8 +68,17 @@ class SumaFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_suma, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_suma, container, false)
+        val botonMulti: Button? = view.findViewById(R.id.suma)
+        val resultadoText: TextView? = view.findViewById(R.id.sumaResultado)
+
+        botonMulti?.setOnClickListener {
+            viewModel.hacerSuma()
+            resultadoText?.text = viewModel.resultado.toString()
+        }
+        return view
 
     }
+
 
 }
