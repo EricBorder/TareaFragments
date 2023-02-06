5bbb73728d16e33b2cf3c2de87629e85774cb92f
EricBorder <magicsergy@gmail.com>
2023-02-06 09:58:54 +0100

    ViewModel comentado

--- app/src/main/java/com/example/tareafragments/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/tareafragments/ui/main/MainViewModel.kt
@@ -7,14 +7,20 @@ import androidx.lifecycle.MutableLiveData
 class MainViewModel(application: Application) : AndroidViewModel(application) {
     // TODO: Implement the ViewModel
 
+    //Creamos variable random de números entre 0 y 9
     var random = (0..9).random()
+    //Creamos variable random2 de números entre 0 y 9
     var random2 = (0..9).random()
+    //Iniciamos el resultado
     var resultado : Int = 0
-
+    //Con esta variable observamos los cambios en el random
     var liveRandom = MutableLiveData<Int>()
+    //Con esta variable observamos los cambios en el random2
     var liveRandom2 = MutableLiveData<Int>()
+    //Con esta variable observamos los cambios en el resultado
     var liveResultado = MutableLiveData<Int>()
 
+    //Inicializamos variables cuando instanciamos
     init {
         liveRandom.value = random
     }
@@ -26,9 +32,15 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
         liveResultado.value = resultado
     }
 
+    /**
+     * Función para sumar dos números aleatorios
+     */
     fun hacerSuma(){
         resultado = random + random2
     }
+    /**
+     * Función para multiplicar dos números aleatorios
+     */
     fun hacerMultiplicacion(){
 
         resultado = random * random2
