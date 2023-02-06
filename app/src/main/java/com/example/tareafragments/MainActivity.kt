2d9bb7d54fbae3aa824c8b31e3e561b219607fe5
EricBorder <magicsergy@gmail.com>
2023-02-06 09:53:49 +0100

    MainActivity comentado

--- app/src/main/java/com/example/tareafragments/MainActivity.kt
+++ app/src/main/java/com/example/tareafragments/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.tareafragments
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -10,25 +11,34 @@ import com.example.tareafragments.ui.main.SumaFragment
 @Suppress("DEPRECATED_IDENTITY_EQUALS")
 class MainActivity : AppCompatActivity() {
 
-   override fun onCreate(savedInstanceState: Bundle?) {
-       super.onCreate(savedInstanceState)
-       setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-       val suma: Button = findViewById(R.id.suma)
-       val multiplicacion: Button = findViewById(R.id.multiplicacion)
+        //Instanciamos el botón de la suma y lo asociamos a su layout
+        val suma: Button = findViewById(R.id.suma)
+        //Instanciamos el botón de la multiplicación y lo asociamos a su layout
+        val multiplicacion: Button = findViewById(R.id.multiplicacion)
 
-       suma.setOnClickListener { replaceFragment(SumaFragment()) }
-       multiplicacion.setOnClickListener { replaceFragment(MultiplicacionFragment()) }
+        //Ponemos un escuchador al botón de la suma y le añadimos la función
+        suma.setOnClickListener { replaceFragment(SumaFragment()) }
+        //Ponemos un escuchador al botón de la multiplicación y le añadimos la función
+        multiplicacion.setOnClickListener { replaceFragment(MultiplicacionFragment()) }
 
 
-   }
-    private fun replaceFragment(fragment: Fragment){
+    }
+
+    /**
+     * Función que cambia de fragmentos en el layout Principal cuando se pulsa el respectivo botón
+     * asociado al respectivo layout de cada Fragment
+     * @param fragment
+     */
+    private fun replaceFragment(fragment: Fragment) {
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.fragmentContainer, fragment)
         fragmentTransaction.addToBackStack(null)
         fragmentTransaction.commit()
     }
-
 
 
 }
